a124ed5099ba76f29bcc91d4b8c29b0ffc68d5c0
sadomohamed <43449772+sadomohamed@users.noreply.github.com>
2018-11-27 10:28:30 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ for (index = element.length - 1; index >= 0; index--) {
 event.stopPropagation();
   });
   addEventListener("click", function(event) {
-    var triangle = document.createElement("div");
-    triangle.className = "dot";
-    triangle.style.left = (event.pageX - 4) + "px";
-    triangle.style.top = (event.pageY - 4) + "px";
+    var dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
     document.body.appendChild(dot);
   });
